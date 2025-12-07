11f4996634e4deac6906ea12256831c8c20498e9
disel <disel@localhost.localdomain>
2025-12-07 11:21:10 +0100

    FIX: Aggiunti import mancanti (MatrixBackground, Skills) in App.tsx

--- guido-diell-portfolio/App.tsx
+++ guido-diell-portfolio/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+import MatrixBackground from './components/MatrixBackground';
 
 function App() {
   return (
